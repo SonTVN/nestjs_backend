ab760d1e0338425d6354b567c29b8cc81cc5f81d
transon.salto <son.tran@salto.vn>
2023-03-07 08:44:42 +0700

    add connect database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostService } from './post/post.service';
+import { PrismaService } from './prisma.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    UserService,
+    PostService,
+    PrismaService,
+  ],
 })
 export class AppModule {}
